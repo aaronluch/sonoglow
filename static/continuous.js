a9e02172e1d4298b21b4d2dd7b2e8c491d9545ee
aaron luciano <119374408+aaronluch@users.noreply.github.com>
2023-12-01 13:57:44 -0500

    cant have the continuous button update site :[

--- static/continuous.js
+++ static/continuous.js
@@ -1,15 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const updateForm = document.getElementById('updateForm'); // Assuming your form has this id
-
-    updateForm.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission
-        callUpdateSong();
-    });
-
-    const continuousButton = document.getElementById('continuousButton');
-    continuousButton.addEventListener('click', toggleContinuousUpdate);
-});
-
 let continuousUpdateEnabled = false;
 let updateInterval;
 
@@ -18,26 +6,27 @@ function toggleContinuousUpdate() {
     document.getElementById('continuousButton').innerText = `CONTINUOUS: ${continuousUpdateEnabled ? 'ENABLED' : 'DISABLED'}`;
 
     if (continuousUpdateEnabled) {
-        updateInterval = setInterval(callUpdateSong, 5000); // Call every 5 seconds
+        updateInterval = setInterval(callUpdateSong, 5000);
     } else {
         clearInterval(updateInterval);
     }
 }
 
 function callUpdateSong() {
-    fetch('/updateSong', {
+    fetch('/updateSong', { 
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
     .then(data => {
         console.log('Update song successful:', data);
-        if (data) {
-            // Update the web page with the new song info
-            document.getElementById('nowPlaying').innerHTML = `Now Playing: ${data.track_name} by ${data.artists}`;
-        }
     })
     .catch(error => {
         console.error('Error during update song:', error);
